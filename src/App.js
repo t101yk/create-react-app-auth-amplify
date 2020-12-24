8c6adf402d03ddb626d4f2c0e971f7f7663cb736
Long <wjljx120@126.com>
2020-12-24 16:07:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.baidu.com"
             target="_blank"
             rel="noopener noreferrer"
           >
